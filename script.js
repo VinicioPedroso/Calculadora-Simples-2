94ad6df9d0bb56a348ec1e3c6166a35b3fd7d00d
Vinicio de Lara Pedroso <168511075+VinicioPedroso@users.noreply.github.com>
2024-05-01 11:32:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ btnDiv.addEventListener('click' ,function (){
     var valor2 = n2.value
 
     if(valor2 === "0"){
-        document.querySelector("#result").innerText = "Não é possível"
+        document.querySelector("#result").innerText = "Eu te amo!"
     }else{
         if (valor1 === "", valor2 ===""){
             document.getElementById('result').innerText = "";
